67448bb3271cf4e3feb9e8ab7c8c9680d057b89a
seanblit18 <seansche@usc.edu>
2025-12-06 23:38:09 -0800

    finally done hw

--- schedwork.cpp
+++ schedwork.cpp
@@ -8,6 +8,7 @@
 #include <algorithm>
 // add or remove necessary headers as you please
 
+
 #endif
 
 #include "schedwork.h"
@@ -21,7 +22,8 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
-
+bool backpack(int day, const int numNeeded, const AvailabilityMatrix& availability,
+                int shift, const int maxshift, DailySchedule& schedule, vector<size_t>& numTaken);
 
 // Add your implementation of schedule() and other helper functions here
 
@@ -37,9 +39,68 @@ bool schedule(
     }
     sched.clear();
     // Add your code below
+    const int numWorkers = avail[0].size(); // total num workers
+    const int numDays = avail.size(); // total num work days
 
 
+    // if no workers, no days to work, or if we need more staff than we have
+    if (numWorkers == 0 || numDays == 0 || static_cast<size_t>(numWorkers) < dailyNeed) {
+        return false;
+    }
 
+
+    sched.assign(numDays, vector<Worker_T>(dailyNeed, INVALID_ID));
+
+    vector<size_t> numTaken(numWorkers, 0);
+
+    bool viable = backpack(0,dailyNeed,avail, 0, maxShifts, sched, numTaken);
+
+    return viable;
 
 }
 
+bool backpack(int day, const int numNeeded, const AvailabilityMatrix& availability,
+    int shift, const int maxshift, DailySchedule& s, vector<size_t>& numTaken)
+{
+    const int numWorkers = availability[0].size(); // total num workers
+    const int numDays = availability.size(); // total num work days
+
+    // finished all days
+    if (numDays == day) {
+        return true;
+    }
+
+    // if today is done, continue to tmrw
+    if (shift == numNeeded) {
+        return backpack(day + 1, numNeeded, availability, 0, maxshift, s, numTaken);
+    }
+
+    for (int i = 0; i < numWorkers; i++) {
+        // skip if unavail
+        if (!availability[day][i]) {continue;}
+
+        // worker can't work more than maxshifts
+        if (!(numTaken[i] < static_cast<size_t>(maxshift))) {continue;}
+
+        // workers only work once a day
+        if (find(s[day].begin(), s[day].begin() + shift,
+            static_cast<Worker_T>(i)) != s[day].begin() + shift) {
+                continue;
+            }
+
+        s[day][shift] = static_cast<Worker_T>(i);
+        numTaken[i]++;
+
+        // move onto next shift
+        if (backpack(day, numNeeded, availability, shift + 1, maxshift, s, numTaken)) {
+            return true;
+        }
+
+        // reversing an assignment or backtracking
+        --numTaken[i];
+        s[day][shift] = INVALID_ID;
+    }
+
+    return false;
+
+}
--- wordle.cpp
+++ wordle.cpp
@@ -13,7 +13,8 @@ using namespace std;
 
 
 // Add prototypes of helper functions here
-
+void oppastoppa(string& curry, unsigned int indy, string floater, const set<string>& dict, set<string>& finalans, int missing);
+bool prunster(const set<string>& dict, const string& pre, size_t n);
 
 // Definition of primary wordle function
 std::set<std::string> wordle(
@@ -22,7 +23,122 @@ std::set<std::string> wordle(
     const std::set<std::string>& dict)
 {
     // Add your code here
+    set<string> final_answers; // set to put validated words with dict
+    string curry = in; // copy of original string
+    string floater = floating; // copy of floating letters
+    size_t missing = 0; // counts num of slots that don't have a fixed letter
+    
+   for (size_t i = 0; i < curry.size(); i++) {
+        if (curry[i] == '-') {
+            missing++;
+        }
+        else {
+            size_t pos = floater.find(curry[i]);
+            if (pos != string::npos) {floater.erase(pos,1);}
+        }
+   }
 
+   // if there are more floating than missing, IMPOSSIBLE
+
+   if (floater.size() > missing) {
+        return final_answers;
+   }
+
+   oppastoppa(curry, 0, floater, dict, final_answers, missing);
+   return final_answers;
 }
 
 // Define any helper functions here
+void oppastoppa(string& curry, unsigned int indy, 
+            string floater, const set<string>& dict, 
+            set<string>& finalans, int missing) 
+{
+
+    // base case --> current word is at n letters and follows constraints
+    if (indy == curry.size()) {
+        // valid iff no floating letters left and found in dict
+        if (floater.empty() && (dict.find(curry) != dict.end())) {
+            finalans.insert(curry);
+        }
+        return;
+    }
+
+    // if fixed, continue
+    if (curry[indy] != '-') {
+        if (curry.size() >= 12) {
+            string prefix = curry.substr(0, indy + 1);
+            if (!prunster(dict, prefix, curry.size())) {
+                return;
+            }
+        }
+        oppastoppa(curry, indy + 1, floater, dict, finalans, missing);
+        return;
+    }
+
+    // curry[indy] atp = '-'
+    // go through floating letters
+    for (size_t i = 0; i < floater.size(); i++) {
+        char sza = floater[i];
+        string kendrick = floater;
+        kendrick.erase(i, 1);
+        curry[indy] = sza;
+
+        bool ihate104 = true;
+        if (curry.size() >= 12) {
+            string prefix = curry.substr(0, indy + 1);
+            if (!prunster(dict, prefix, curry.size())) {
+                ihate104 = false;
+            }
+        }
+       
+        if (ihate104) {
+            oppastoppa(curry, indy + 1, kendrick, dict, finalans, missing - 1);
+        }        
+        curry[indy] = '-';
+    }
+
+    // brute force remaining letters iff there are enough spots for any remaining floaters
+    if (static_cast<int>(floater.size()) < missing) {
+        for (char c = 'a'; c <= 'z'; c++) { // hehe c++
+            if (floater.find(c) != string::npos) {
+                continue;
+            }
+            curry[indy] = c;
+
+            bool ilove104 = true;
+            if (curry.size() >= 12) {
+                string prefix = curry.substr(0, indy + 1);
+                if (!prunster(dict, prefix, curry.size())) {
+                    ilove104 = false;
+                }
+            }
+
+            if (ilove104) {
+                oppastoppa(curry, indy + 1, floater, dict, finalans, missing - 1);
+            }
+            curry[indy] = '-';
+        }
+    }
+}
+
+bool prunster(const set<string>& dict, const string& pre, size_t n) 
+{
+    set<string>::const_iterator it = dict.begin();
+    while (it != dict.end()) {
+        if (it->size() == n) {
+            bool check = true;
+            for (size_t i = 0; i < pre.size(); i++) {
+                if (i >= it->size() || (*it)[i] != pre[i]) {
+                    check = false;
+                    break;
+                }
+            }
+            if (check) {
+                return true;
+            }
+        }
+        ++it;
+    }
+    return false;
+}
+
